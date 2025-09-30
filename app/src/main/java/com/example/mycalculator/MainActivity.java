3f71faf386727abc4a3b65851e367cb4cbe0e4d7
AnonymAsif <asifisawesome6474@gmail.com>
2025-09-30 00:43:16 -0400

    Complete basic functionality. Added budget javadoc.

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -1,85 +1,113 @@
 package com.example.mycalculator;
-import net.objecthunter.exp4j.Expression;
-import net.objecthunter.exp4j.ExpressionBuilder;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btn1, btn2, btn_clear, btn_plus, btn_equal;
+    /**
+     * Flag text for invalid computation
+     */
+    private static String ERROR_FLAG = "Error!";
+
+    /**
+     * List of symbols supported by buttons. Parallel array to symbolButtons.
+     */
+    String[] symbols;
+
+    /**
+     * List of buttons corresponding to each symbol is symbols
+     */
+    Button[] symbolButtons;
+    Button equalsButton, clearButton;
+
+    /**
+     * Number display for computation results
+     */
     TextView text_display;
 
-    // This is to evaluate the math expression
-
+    /**
+     * Creates Activity by initializing buttons and text field
+     * @param savedInstanceState Activity state variable
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Parallel arrays of symbols and their respective buttons
+        symbols = new String[] {
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", "."};
 
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn_plus = (Button) findViewById(R.id.btn_plus);
-        btn_equal = (Button) findViewById(R.id.btn_equal);
-        btn_clear = (Button) findViewById(R.id.btn_clear);
+        // Button IDs for numbers and operations
+        int[] symbolButtonIDs = new int[] {R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3,
+                R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9,
+                R.id.btn_plus, R.id.btn_minus, R.id.btn_mult, R.id.btn_div, R.id.btn_dot};
+
+        // Initializes arrays of buttons
+        symbolButtons = new Button[symbolButtonIDs.length];
+        for (int i = 0; i < symbolButtons.length; i++)
+            symbolButtons[i] = (Button) findViewById(symbolButtonIDs[i]);
+
+        // Initializes other buttons and text view
+        clearButton = (Button) findViewById(R.id.btn_clear);
+        equalsButton = (Button) findViewById(R.id.btn_equals);
         text_display = (TextView) findViewById(R.id.textview_input_display);
 
         setClickListeners();
     }
 
+    /**
+     * Adds the onClickListeners for each button on the calculator
+     */
     private void setClickListeners() {
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn_plus.setOnClickListener(this);
-        btn_equal.setOnClickListener(this);
-        btn_clear.setOnClickListener(this);
-    }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn1:
-                addNumber("1");
-                break;
-            case R.id.btn2:
-                addNumber("2");
-                break;
-            case R.id.btn_plus:
-                addNumber("+");
-                break;
-            case R.id.btn_equal:
-                String result = null;
-                try {
-                    result = evaluate(text_display.getText().toString());
-                    text_display.setText(result);
-                } catch (Exception e) {
-                    text_display.setText("Error");
-                }
-                break;
-            case R.id.btn_clear:
-                clear_display();
-                break;
+        // Sets the action of symbol buttons to adding the symbol to text box
+        for (int i = 0; i < symbolButtons.length; i++) {
+            int index = i;
+            symbolButtons[index].setOnClickListener(v -> addSymbol(symbols[index]));
         }
+
+        // Logic for other buttons
+        clearButton.setOnClickListener(v -> clear_display());
+        equalsButton.setOnClickListener(v -> {
+            try { text_display.setText(
+                    evaluate(text_display.getText().toString()));}
+            catch (RuntimeException e) {text_display.setText(ERROR_FLAG);}
+        });
     }
 
-    private String evaluate(String expression) throws Exception {
-        String result = evaluate(expression);
+    /**
+     * Parses and evaluates an arithmetic expression string
+     * @param expression The expression string
+     * @return The evaluated result
+     */
+    private String evaluate(String expression) {
+        String result = MathEval.eval(expression);
         BigDecimal decimal = new BigDecimal(result);
-        return decimal.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString();
+        return decimal.setScale(2, RoundingMode.HALF_UP).toPlainString();
     }
 
-    private void addNumber(String number) {
-        text_display.setText(text_display.getText() + number);
+    /**
+     * Adds a symbol to the end of the text field
+     * @param symbol The symbol to be added
+     */
+    private void addSymbol(String symbol) {
+        CharSequence currentText = text_display.getText();
+        if (currentText.equals(ERROR_FLAG))
+            text_display.setText(symbol);
+        else text_display.setText(currentText + symbol);
     }
 
+    /**
+     * Clears the display by emptying text field
+     */
     private void clear_display() {
         text_display.setText("");
     }
